cbbea6363c636f2d9f2d74a948bd71656ccabcae
Jordan <100964858+j0j032@users.noreply.github.com>
2022-10-06 16:43:45 +0200

    ✨ Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import logo from '../../assets/img/argentBankLogo.png'
+import {NavLink} from 'react-router-dom'
 const Header = () => {
 	return (
-		<header>
-			HEAD
+		<header className='header__container'>
+			<NavLink to='/'>
+				<img className='header__logo' src={logo} alt='logo'/>
+			</NavLink>
+			<NavLink className='header__nav-container' to='/signin'>
+				<i className="fa fa-user-circle"></i>
+				<p>Sign In</p>
+			</NavLink>
 		</header>
 	)
 }
