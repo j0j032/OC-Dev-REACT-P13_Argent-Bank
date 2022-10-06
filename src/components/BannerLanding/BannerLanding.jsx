8a794c1f3c2da840311180dd8e70682449b21f4a
Jordan <100964858+j0j032@users.noreply.github.com>
2022-10-06 17:06:02 +0200

    ✨ Banner

--- src/components/BannerLanding/BannerLanding.jsx
+++ src/components/BannerLanding/BannerLanding.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const BannerLanding = () => {
 	return (
-		<div>
-			Banner
+		<div className='banner'>
+			<section className='banner__content'>
+				<h2>No fees</h2>
+				<h2>No minimum deposit</h2>
+				<h2>High interest rates</h2>
+				<p>Open a savings account with Argent Bank today!</p>
+			</section>
 		</div>
 	)
 }
