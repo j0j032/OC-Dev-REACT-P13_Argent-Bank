f8bdac3969cf5d1b041427f549c42f08f7222d03
Jordan <100964858+j0j032@users.noreply.github.com>
2022-10-06 17:49:10 +0200

    ✨ Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 const Footer = () => {
+	
+	const date = Date.now()
+	
+	/**
+	 * To get the good copyright year each year
+	 * @param {number} date
+	 * @returns {string}
+	 */
+	const dateFormater = date => new Date(date).toLocaleDateString('fr-FR', {year: 'numeric'})
+	
 	return (
-		<footer>
-			FOOT
+		<footer className='footer'>
+			<p>{`Copyright ${dateFormater(date)} Argent Bank`}</p>
 		</footer>
 	)
 }
