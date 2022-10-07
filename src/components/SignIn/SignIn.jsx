b87841608ab6cac8257cfe6b929284a710bac108
Jordan <100964858+j0j032@users.noreply.github.com>
2022-10-07 11:10:18 +0200

    ✨ signIn Page: Style without login logic

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+import {useNavigate} from 'react-router-dom'
+import useBoolean from '../../customHooks/UseBoolean'
 
 const SignIn = () => {
+	const [isToggle, {setToggle}] = useBoolean(false)
+	
+	const navigateTo = useNavigate()
+	const handleSubmit = e => {
+		e.preventDefault()
+		// Auth Logic
+		// Remember LOGIC
+		navigateTo('/profile')
+	}
+	
 	return (
-		<div>
-			SIGNIN
-		</div>
+		<>
+			<Header/>
+			<main className='main signin__bg-dark'>
+				<section className='signin__container'>
+					<i className='fa fa-user-circle sign-in-icon'></i>
+					<h1>Sign In</h1>
+					<form className='signin__form'
+					      onSubmit={(e)=>handleSubmit(e)}>
+						<div className='input__wrapper'>
+							<label htmlFor='username'>Username</label>
+							<input type='text' id='username'/>
+						</div>
+						<div className='input__wrapper'>
+							<label htmlFor='password'>Password</label>
+							<input type='password' id='password'/>
+						</div>
+						<div className='input__remember'>
+							<input onClick={setToggle} defaultChecked={isToggle} type='checkbox' id='remember-me'/>
+							<label htmlFor='remember-me'>Remember me</label>
+						</div>
+						<button className='signin__btn'>Sign In</button>
+					</form>
+				</section>
+			</main>
+			<Footer/>
+		</>
 	)
 }
 
